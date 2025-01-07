15b00d68ead3f5d501cca73345d12c186c99cf99
golkelj <jadengoelkel@gmail.com>
2025-01-06 20:52:35 -0800

    addedTheLab

---
+++ labs/lab00.py
@@ -0,0 +1,22 @@
+"""
+DSC 20 Lab 00
+Name: TODO
+PID:  TODO
+"""
+
+def respect_your_cat(food, num):
+    """
+    This method is a demand from your cats to provide food for them.
+    If the food is strictly less than 5 characters, this method will 
+    return "Give me more food!" Otherwise return the food as a string
+    followed by 'num' # of exclamation marks.
+
+    >>> respect_your_cat("dog", 5)
+    'Give me more food!'
+    >>> respect_your_cat("lots of fish and lots of fish", 4)
+    'lots of fish and lots of fish!!!!'
+    >>> respect_your_cat("Fruits", 0)
+    'Fruits'
+    """
+    # YOUR CODE GOES HERE #
+    return
